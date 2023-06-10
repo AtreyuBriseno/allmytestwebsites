e61a6c9fb6fd3c70902bb297eef8d7d7f5ab6185
AtreyuBriseno <100451379+AtreyuBriseno@users.noreply.github.com>
2023-06-09 19:25:52 -0700

    Created srcforAVFD.js

---
+++ srcforAVFD.js
@@ -0,0 +1,13 @@
+document.getElementById("submit").onclick = function(){
+  var sa = document.getElementById("age").value    //sa means submitidage
+  }
+  
+if (sa < 15){
+    alert('too young')
+}if (sa = 15){
+  alert('too young but you can try for your permit')
+}if (sa >= 70){
+  alert('you might need to get a new permit')
+}else{
+  you an drive
+}
