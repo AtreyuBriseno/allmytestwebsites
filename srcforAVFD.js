dd7fdea543526972414808b21afbc8a08284989c
AtreyuBriseno <100451379+AtreyuBriseno@users.noreply.github.com>
2023-06-09 19:54:02 -0700

    Update srcforAVFD.js
    now it should work for "22" error

--- srcforAVFD.js
+++ srcforAVFD.js
@@ -1,9 +1,9 @@
 document.getElementById("submit").onclick = function(){
   var sa = document.getElementById("age").value;    //sa means submitidage
   
-  if (sa < 15 and sa !> 16){
+  if (sa < 15){
     alert('too young');
-  }if (sa = 15){
+  }if (sa = 15 and sa !> 16 ){
     alert('too young but you can try for your permit');
   }if (sa >= 70){
     alert('you might need to get a new permit');
