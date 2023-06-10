c10e16d55c7432f6c1d0b6f68e48f6b9e3748f9c
AtreyuBriseno <100451379+AtreyuBriseno@users.noreply.github.com>
2023-06-09 20:52:24 -0700

    goes back to previos working code with some updates AVFD

--- srcforAVFD.js
+++ srcforAVFD.js
@@ -1,3 +1,4 @@
+/*
 document.getElementById("submit").onclick = function(){
   var sa = document.getElementById("age").value;    //sa means submitidage
   
@@ -11,4 +12,17 @@ document.getElementById("submit").onclick = function(){
     alert('you can drive')
   }
 }
-  
+*/
+document.getElementById("submit").onclick = function(){
+  var sa = document.getElementById("age").value;    //sa means submitidage
+
+  if (sa < 15){
+    alert('too young');
+  }if (sa = 15 and sa < 16){
+    alert('too young but you can try for your permit');
+  }if (sa >= 70){
+    alert('you might need to get a new permit');
+  }else{
+    alert('you can drive');
+  }
+}
